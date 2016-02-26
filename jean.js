827f626d0af1d7214cfdf39d835d4259b2c8517b
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-02-26 23:18:01 +1300

    Add blinking name

--- jean.js
+++ jean.js
@@ -1,2 +1,13 @@
 // Jean's javascript goes here
 console.log( 'hello!' );
+
+$( document ).ready( function() {
+    setInterval( blink, 1000 );
+});
+
+function blink() {
+    $( '.developer-card.jean h1' )
+        .addClass( 'red' )
+        .fadeTo( 'slow', 0.5 )
+        .fadeTo( 'slow', 1.0 );
+}
