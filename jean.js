8ebd84bd6a331b7951584fd4480fadff456c8b6f
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-02-28 23:09:07 +1300

    Add bouncing

--- jean.js
+++ jean.js
@@ -1,12 +1,29 @@
 // Jean's javascript goes here
 
+var h1 = null;
+
 $( document ).ready( function() {
+    h1 = $( '.developer-card.jean h1' );
     setInterval( blink, 1000 );
+    jump();
 });
 
 function blink() {
-    $( '.developer-card.jean h1' )
-        .addClass( 'red' )
+    h1.addClass( 'red' )
         .fadeTo( 'slow', 0.5 )
         .fadeTo( 'slow', 1.0 );
+}
+
+function jump() {
+    var letters = h1.lettering();
+    var spans = letters[ 0 ].children;
+
+    $.each( spans, function( i, val ) {
+        if ( typeof parseInt( i ) !== NaN ) {
+            $( spans[ i ] ).delay( 300 * i ).queue( function( next ) {
+                $( this ).addClass( 'jump' );
+                next();
+            });
+        }
+    });
 }
